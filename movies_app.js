cb26b951c2dc5835777926dea52658efb8c710db
Diogo Botelho <diogofilipebotelho@gmail.com>
2021-08-30 21:04:49 -0400

    Finished movies app with a tiny bug

--- movies_app.js
+++ movies_app.js
@@ -9,9 +9,13 @@
     $("#movieList").append(`<li> ${movieName} : ${movieRating}` )
 })*/
 
-$("#submitButton").on("click", function(){
+$("#submitButton").on("click", function () {
     const movieName = $("#movieName").val()
     const movieRating = $("#movieRating").val()
     console.log(`this works. movie name: ${movieName} movie rating: ${movieRating}`)
-    $("#movieList").append(`<li> ${movieName} : ${movieRating} ---- <input type="submit" value="remove">`)
+    $("#movieList").append(`<li id=${movieName}> ${movieName} : ${movieRating} ---- <input type="submit" value="remove" class="removeButton">`)
+})
+
+$('#movieList').on("click", "li", function (evt) {
+    evt.target.remove();
 })
